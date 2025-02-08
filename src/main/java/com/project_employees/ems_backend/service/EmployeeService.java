10509a68ddee787c6ae5dfcfffd306acd21f2f9d
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-08 19:28:06 -0300

    Agrega getEmployeeById a EmployeeService y metodos a EmployeeServiceImpl

--- src/main/java/com/project_employees/ems_backend/service/EmployeeService.java
+++ src/main/java/com/project_employees/ems_backend/service/EmployeeService.java
@@ -4,4 +4,6 @@ import com.project_employees.ems_backend.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
 }
