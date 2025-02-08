de8983f3e32efcbf5a5b67630c977791b3c1ceb3
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-08 18:23:33 -0300

    Agrega EmployeeService a servicios

---
+++ src/main/java/com/project_employees/ems_backend/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.project_employees.ems_backend.service;
+
+import com.project_employees.ems_backend.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
