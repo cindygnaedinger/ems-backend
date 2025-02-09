4631c0813300fe927f7e93d28b564d19910247e7
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-09 08:17:04 -0300

    Agrega buscar todos los empleados y modificar empleado REST API

--- src/main/java/com/project_employees/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/project_employees/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,15 @@ package com.project_employees.ems_backend.service.impl;
 
 import com.project_employees.ems_backend.dto.EmployeeDto;
 import com.project_employees.ems_backend.entity.Employee;
+import com.project_employees.ems_backend.exception.ResourceNotFoundException;
 import com.project_employees.ems_backend.mapper.EmployeeMapper;
 import com.project_employees.ems_backend.repository.EmployeeRepository;
 import com.project_employees.ems_backend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -23,6 +27,30 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
-        return null;
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exists with the given id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee with the given id does not exists: " + employeeId)
+        );
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
     }
 }
